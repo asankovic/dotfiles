29a8751477528b0d0d64928ca31dd6f66426c181
Alen Sankovic <alen5.sankovic@gmail.com>
2025-04-08 18:40:18 +0200

    Add wezterm config

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,24 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- Theme & Fonts
+local custom = wezterm.color.get_builtin_schemes()['Catppuccin Mocha']
+custom.background = '#11111B'
+config.color_schemes = {
+	['DarkCatppuccinMocha'] = custom,
+}
+config.color_scheme = 'DarkCatppuccinMocha'
+config.font_size = 11.0
+config.font = wezterm.font 'FiraCode Nerd Font'
+
+-- Window
+config.enable_tab_bar = false
+config.window_close_confirmation = 'NeverPrompt'
+-- config.window_decorations = 'RESIZE'
+config.default_prog = { '/usr/bin/zsh' }
+
+return config
