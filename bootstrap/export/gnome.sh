c4d22d826802c79fb221a3ee3f5573d218a62cc8
Alen Sanković <alen5.sankovic@gmail.com>
2025-05-04 18:13:50 +0200

    feat(dconf): Add dconf import and export

---
+++ bootstrap/export/gnome.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+CHEZMOI_GNOME_TRACK_DIR="$(chezmoi source-path)/bootstrap/track/gnome"
+
+# dconf
+dconf dump /org/gnome/desktop/wm/ > "$CHEZMOI_GNOME_TRACK_DIR/wm.ini"
+dconf dump /org/gnome/desktop/background/ > "$CHEZMOI_GNOME_TRACK_DIR/background.ini"
+dconf dump /org/gnome/desktop/screensaver/ > "$CHEZMOI_GNOME_TRACK_DIR/screensaver.ini"
+dconf dump /org/gnome/desktop/interface/ > "$CHEZMOI_GNOME_TRACK_DIR/interface.ini"
+dconf dump /org/gnome/shell/ > "$CHEZMOI_GNOME_TRACK_DIR/shell.ini"
