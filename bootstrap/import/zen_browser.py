475b2f16a869c71c039347e105495562460bb03a
Alen Sanković <alen5.sankovic@gmail.com>
2025-05-04 15:44:06 +0200

    feat(zen): Copy zen theme only on change

--- bootstrap/import/zen_browser.py
+++ bootstrap/import/zen_browser.py
@@ -4,6 +4,7 @@ import sys
 import subprocess
 import configparser
 import shutil
+import filecmp
 
 def get_chezmoi_track_dir_path():
     # have to hardcode path because two chezmoi commands cannot run at the same time (apply and source-path)
@@ -25,15 +26,21 @@ def get_zen_themes_dir_path():
 
     return os.path.join(zen_profile_path, "chrome")
 
-def import_file(file_name, chezmoi_dir, zen_dir):
+def import_file_if_changed(file_name, chezmoi_dir, zen_dir):
     source_file = os.path.join(chezmoi_dir, file_name)
     destination_file = os.path.join(zen_dir, os.path.basename(source_file))
 
-    if os.path.isfile(source_file):
-        shutil.copy(source_file, destination_file)
-        print(f"Copied {source_file} to {destination_file}")
-    else:
+    if not os.path.isfile(source_file):
         print(f"Missing expected theme file: {source_file}", file=sys.stderr)
+        return 1
+
+    if os.path.exists(destination_file) and filecmp.cmp(source_file, destination_file):
+        return 2
+
+    shutil.copy(source_file, destination_file)
+    print(f"Copied {source_file} to {destination_file}")
+
+    return 0
 
 
 def main():
@@ -50,7 +57,18 @@ def main():
 
     files_to_import = ["userChrome.css", "userContent.css", "zen-logo-mocha.svg"]
 
+    # 0 = changed
+    # 1 = error
+    # 2 = no change
+    status = 2
+
     for file_name in files_to_import:
-        import_file(file_name, chezmoi_track_dir, zen_themes_dir)
+        changed = import_file_if_changed(file_name, chezmoi_track_dir, zen_themes_dir)
+        if changed == 1:
+            status = 1
+        elif changed == 0:
+            status = 0
+
+    sys.exit(status)
 
 main()
