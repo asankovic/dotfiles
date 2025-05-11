56dab49ac5aff9533a92216634397a7da15630c1
Alen Sanković <alen5.sankovic@gmail.com>
2025-05-11 14:30:34 +0200

    feat(gnome): track and import plugins dconf

--- bootstrap/export/gnome.sh
+++ bootstrap/export/gnome.sh
@@ -10,6 +10,7 @@ dconf dump /org/gnome/desktop/wm/ > "$CHEZMOI_GNOME_TRACK_DIR/wm.ini"
 dconf dump /org/gnome/desktop/background/ > "$CHEZMOI_GNOME_TRACK_DIR/background.ini"
 dconf dump /org/gnome/desktop/screensaver/ > "$CHEZMOI_GNOME_TRACK_DIR/screensaver.ini"
 dconf dump /org/gnome/desktop/interface/ > "$CHEZMOI_GNOME_TRACK_DIR/interface.ini"
+dconf dump /org/gnome/settings-daemon/plugins/ > "$CHEZMOI_GNOME_TRACK_DIR/plugins.ini"
 dconf dump /org/gnome/shell/ > "$CHEZMOI_GNOME_TRACK_DIR/shell.ini"
 
 echo "Exported gnome extension list and dconfs to '$CHEZMOI_GNOME_TRACK_DIR'"
